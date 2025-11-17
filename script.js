0d7c162536fd2fa964ed510cb25584c6bf8bfcc7
Kustod <Felix.Iskra@seznam.cz>
2025-11-17 10:47:55 +0100

    cv_pattern

--- script.js
+++ script.js
@@ -8,6 +8,38 @@ window.onload = function() {
             item.style.transform = 'translateY(0)';
         }, 200);
     });
+    document.querySelectorAll('.member-item').forEach(item => {
+        item.style.opacity = '0';
+        item.style.transform = 'translateY(20px)';
+        setTimeout(() => {
+            item.style.transition = 'all 0.5s ease';
+            item.style.opacity = '1';
+            item.style.transform = 'translateY(0)';
+        }, 200);
+    });
+    document.querySelectorAll('.cv-item').forEach(item => {
+        item.style.opacity = '0';
+        setTimeout(() => {
+            item.style.transition = 'all 0.5s ease';
+            item.style.opacity = '1';
+        }, 200);
+    });
+};
+
+function display_cv(x){
+    document.querySelectorAll(x).forEach(item => {
+        if (item.style.display == 'flex') {
+            item.style.display = 'none';
+        }
+        else {
+            item.style.display = 'flex'; 
+            item.style.opacity = '0';
+                setTimeout(() => {
+                item.style.transition = 'all 0.5s ease';
+                item.style.opacity = '1';
+            }, 1);
+        }
+    });
 };
 
 function myFunction() {
